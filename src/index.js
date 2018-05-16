2a2c66f5f9798667b77caf2e41068cc2e0652b9b
David Morgan <dmorgan81@gmail.com>
2018-05-16 14:03:35 -0400

    Faction chart

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import get from './utils/get';
 import crossfilter from 'crossfilter2';
 import dc from 'dc';
 import 'dc/dc.css';
-import './components/set-number';
-import SetNumber from './components/set-number';
+import SetChart from './components/set-chart';
+import FactionChart from './components/faction-chart';
 
 get('./eternal-cards.json').then(cards => {
+  cards = cards.map(card => {
+    card.Factions = Array.from(new Set(card.Influence.match(/{[FJPST]}/gi)));
+    return card;
+  });
   let data = crossfilter(cards);
-  let setNumber = new SetNumber(data, '#set-number');
+  let setChart = new SetChart(data, '#set-chart');
+  let factionChart = new FactionChart(data, '#faction-chart');
 
   dc.renderAll();
 });
